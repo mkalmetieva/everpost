0c61d70e08379121c60c189b6a912e1612a4d4bb
Marina <looking4h8@gmail.com>
2016-12-28 13:37:07 +0200

    Admin views added

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from main.models import Post, Comment
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'created_at')
+    search_fields = ('title', 'author__username')
+
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('author', 'text', 'created_at')
+    search_fields = ('author__username', 'text')
+
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Comment, CommentAdmin)
